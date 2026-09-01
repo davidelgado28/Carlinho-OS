c637b39b4b7d5991b55e38d14f33b530c7a3ba74
David Carlos <davidcarlosdelgado28@gmail.com>
2026-08-31 21:31:07 -0300

    Create carlinho.h

---
+++ lang/include/carlinho.h
@@ -0,0 +1,16 @@
+#ifndef CARLINHO_H
+#define CARLINHO_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+
+namespace facilitador {
+    using namespace std;
+}
+
+#endif
